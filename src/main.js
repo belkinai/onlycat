c8f860e7cfb81343932fabc4115a4253d71ea95b
Alexandr Belkin <msveet@gmail.com>
2020-10-01 20:03:29 +0700

    base

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
+import './assets/common.css';
+import 'normalize.css';
